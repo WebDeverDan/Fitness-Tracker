e6609cabfa038691d2b738e55c8d89f77b7a8fbc
Daniel Bradley <danielbvideo@gmail.com>
2021-10-13 10:19:07 -0400

    modified workout with exercise array

--- models/workout.js
+++ models/workout.js
@@ -1,14 +1,47 @@
 const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
-const exerciseSchema = require('./exercise')
+
 
 const workoutSchema = new Schema({
   day: {
     type: Date,
     default: Date.now
   },
-  exercise:{ exerciseSchema }
+  exercise:[
+    {
+    type: {
+      type: String,
+      trim: true,
+      required: "type is Required"
+    },
+    name: {
+      type: String,
+      trim: true,
+      required: "type is Required"
+    },
+    duration: {
+      type: Number,
+      unique: true,
+      required: true
+    },
+    weight: {
+      type: Number,
+      unique: true,
+      required: false
+    },
+    reps: {
+      type: Number,
+      unique: true,
+      required: false
+    },
+    sets: {
+      type: Number,
+      unique: true,
+      required: false
+    },
+    }
+],
 });
 
 const Workout = mongoose.model("Workout", workoutSchema);
